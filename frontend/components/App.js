331f3d77103a1579211fbdadf34521f4c1d55f7a
Tommy Duong <tduong0715@gmail.com>
2024-03-23 00:03:44 -0700

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,14 +6,15 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state = {
     todos: [],
-  }
+    errorL '',
+  } 
   fetchAllTodos = () => {
     axios.get(URL)
     .then(res => {
       this.setState({ ...this.state, todos: res.data.data })
     })
     .catch(err => {
-      debugger
+      this.setState({ ...this.state, error: err.response.data.message })
     })
   }
   componentDidMount() {
@@ -22,7 +23,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div id="error">Error: No error here</div>
+        <div id="error">Error: {this.state.error}</div>
         <div id="todos">
           <h2>Todos:</h2>
           {
