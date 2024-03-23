64dacbf8b747ca8fbf7afb0cd88d5faf1a0ecab7
Tommy Duong <tduong0715@gmail.com>
2024-03-23 00:12:44 -0700

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,8 +6,13 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state = {
     todos: [],
-    errorL '',
-  } 
+    error: '',
+    todoNameInput: '',
+  }
+  onTodoNameInputChange = evt => {
+    const { value } = evt.target
+    this.setState({ ...this.state, todoNameInput: value })
+  }
   fetchAllTodos = () => {
     axios.get(URL)
     .then(res => {
@@ -33,7 +38,7 @@ export default class App extends React.Component {
           }
         </div>
         <form id="todoForm">
-          <input type="text" placeholder="Type todo"></input>
+          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder="Type todo"></input>
           <input type="submit"></input>
           <button>Clear Completed</button>
         </form>
